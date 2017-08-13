3c744d2b2f69ff1d15e8e3be55c007b074c59ce9
triestpa <patrick.triest@gmail.com>
2017-08-13 17:04:45 +0700

    Add additional samples to html page

--- public/app.js
+++ public/app.js
@@ -30,7 +30,7 @@ const vm = new Vue ({
     },
   },
   async created () {
-    this.baseUrl = 'http://localhost:3000/generate'
+    this.baseUrl = 'http://localhost:3000/'
     this.generate()
   },
   methods: {
